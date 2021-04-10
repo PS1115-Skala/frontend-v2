8574361d6326d60c2a11d767c36a3717a6dae6b5
Jose Barrera <mrjrbarreram@gmail.com>
2021-04-09 22:02:31 -0400

    refactor with forkjoin

--- src/app/features/laboratories/pages/list-laboratories-page/list-laboratories-page.component.ts
+++ src/app/features/laboratories/pages/list-laboratories-page/list-laboratories-page.component.ts
@@ -3,6 +3,7 @@ import { Room } from "../../../dashboard/models/room";
 import { AdminLab } from "../../models/adminLab";
 import { DashboardService } from "../../../dashboard/services/dashboard.service";
 import { LaboratoriesService } from "../../services/laboratories.service";
+import { forkJoin } from "rxjs";
 
 @Component({
   selector: 'app-list-laboratories-page',
@@ -13,37 +14,35 @@ export class ListLaboratoriesPageComponent implements OnInit {
 
   public allRooms: Room[] = [];
   public allAdmins: AdminLab[] = [];
-  private waitForIt: number = 0;
-  private numToWait: number = 2;
   private roomGroupByAdmin  = {};
+
   constructor(
     private dashboardService: DashboardService,
     private laboratoriesService: LaboratoriesService,
   ) { }
 
-  laboratorios: any[];
   ngOnInit(): void {
-    this.laboratorios = [1,2,3,4,5];
-    this.dashboardService.getAllRooms().subscribe((rooms: Room[]) => {
-      this.allRooms = [...rooms];
-      this.waitForIt += 1; 
+
+    let rooms = this.dashboardService.getAllRooms();
+    let admins = this.laboratoriesService.getAdminLabs();
+
+    forkJoin({
+      rooms: rooms,
+      admins: admins,
+    }).subscribe(({ rooms, admins }) => {
+      this.allRooms = rooms;
+      this.allAdmins = admins;
       this.createListOfRoomGroupByAdmin();
     });
-    this.laboratoriesService.getAdminLabs().subscribe((adminsList: AdminLab[]) => {
-      this.allAdmins = [...adminsList];
-      this.waitForIt += 1; 
-      this.createListOfRoomGroupByAdmin();
-    });
+
   }
 
   createListOfRoomGroupByAdmin(){
-    if (this.numToWait == this.waitForIt){
-      this.allAdmins.forEach(element => {
-        this.roomGroupByAdmin[element.id] = [];
-      });
-      this.allRooms.forEach(element => {
-        this.roomGroupByAdmin[element.manager_id].push(element);
-      });
-    }
+    this.allAdmins.forEach(element => {
+      this.roomGroupByAdmin[element.id] = [];
+    });
+    this.allRooms.forEach(element => {
+      this.roomGroupByAdmin[element.manager_id].push(element);
+    });
   }
 }
